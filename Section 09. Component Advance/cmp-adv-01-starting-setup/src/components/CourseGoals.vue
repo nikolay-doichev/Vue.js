d87c6acf9d5dbecb5f60049e084d64bb2f31d6ed
Nikolay Doychev <nikolay.doichev@gmail.com>
2020-10-14 14:52:56 +0300

    Global and Local Components, Scoped Styles, Dynamic Components and teleportating elements.

---
+++ Section 09. Component Advance/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+      <li v-for="goal in goals" :key="goal">
+          <slot :item='goal' another-props='...'>{{goal}}</slot>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Finishe the course', 'Learn Vue']
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
