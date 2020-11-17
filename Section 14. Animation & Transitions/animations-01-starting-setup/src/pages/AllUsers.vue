69639f0107120078bee12f73c6e3b626624e9f71
Nikolay Doychev <nikolay.doichev@gmail.com>
2020-11-17 17:08:50 +0200

    Animation & Transitions. Animations Basics & CSS Transitions. Transition Component. Animating Modal. Transition Events. Animate List item. Animating Route Changes.

---
+++ Section 14. Animation & Transitions/animations-01-starting-setup/src/pages/AllUsers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <h2>All Users</h2>
+      <router-link to="/goals">Course Goals</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
